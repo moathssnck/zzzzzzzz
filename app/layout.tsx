fd1d0b9fedb016e9f6c2677a8c2f0b4ba4680b3a
moathssnck <myneww00@gmail.com>
2025-12-20 02:00:27 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
+import Script from "next/script"
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -16,6 +17,16 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <Script src="https://www.googletagmanager.com/gtag/js?id=AW-16965660139" strategy="afterInteractive" />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'AW-16965660139');
+          `}
+        </Script></head>
       <body className={inter.className}>{children}</body>
     </html>
   );
